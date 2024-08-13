fbbd4c867509a639a0d785c67b11e30ad7d72083
shani agnihotri <shaniagnihotri@shanis-MacBook-Air.local>
2024-08-13 20:30:40 +0530

    new design in Readme

--- DB.py
+++ DB.py
@@ -3,34 +3,34 @@ from boto3.dynamodb.conditions import Attr ,  Key
 
 table = resource('dynamodb').Table('Users')
 
-def insert_user():
+def insert_user(Item):
     response= table.put_item(
-
-        Item={
-            
-        "userid":"6"
-        ,
-        "Address":"sagar",
-        "FirstName": "amanishan",
-        "flat": ""
-        ,
-        "FlatVisit": "204"
-        ,
-        "LastName": "Kumar"
-        ,
-        "LoginType": "Visitor"
-        ,
-        "Password": ""
-        ,
-        "Phone":7000898974
-        ,
-        "Username": ""
-      }
+              Item=Item
+      ##  Item={
+       #     
+       # "userid":"6"
+       # ,
+       # "Address":"sagar",
+       # "FirstName": "amanishan",
+       # "flat": ""
+       # ,
+       # "FlatVisit": "204"
+       # ,
+       # "LastName": "Kumar"
+       # ,
+       # "LoginType": "Visitor"
+       # ,
+       # "Password": ""
+       # ,
+       # "Phone":7000898974
+       # ,
+       # "Username": ""
+      #}
 
         
     )
     return response
 
 
-result=insert_user()
-print(result)
+#result=insert_user()
+#print(result)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask , render_template, request
+import DB
+from Users import Users as user
 
 app = Flask(__name__)
 
@@ -9,16 +11,21 @@ def printword(word):
 
 
 @app.route('/',methods=["GET","POST"])
-def hello():
+def insertVisitor():
+    loginType="Visitor"
     if request.method=="POST":
+        userid=request.form["userid"]
         firstname=request.form["firstname"]
         lastname=request.form.get("lastname")
         address=request.form.get("address")
         Phone=request.form.get("phone")
         visitflat=request.form.get("visitflat")
-        printword(firstname)
+        Item=user(userid,firstname,lastname,address,loginType,visitflat,Phone)
+        Items=Item.visitior_to_dict()
+        print(Items)
+        DB.insert_user(Items)
     return render_template('/index.html')
 
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(port=5000,debug=True)
